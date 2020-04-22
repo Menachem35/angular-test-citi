f1ae7dfb508b08a7591dd4437c1b015146b218c4
Menachem35 <menachemglik@yahoo.com>
2020-04-22 11:16:11 -0400

    Responsive fixes, ag-grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { AgGridModule } from 'ag-grid-angular';
+
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { MainViewComponent } from './main-view/main-view.component';
 import { BuySellComponent } from './buy-sell/buy-sell.component';
 import { CoinsAppService } from './coins-app-service.service';
 import { PriceStylingPipe } from './price-styling.pipe';
+import { TransactionGridComponent } from './transaction-grid/transaction-grid.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, MainViewComponent, BuySellComponent, PriceStylingPipe ],
+  imports:      [ BrowserModule, FormsModule, AgGridModule.withComponents([]), ],
+  declarations: [ AppComponent, HelloComponent, MainViewComponent, BuySellComponent, PriceStylingPipe, TransactionGridComponent ],
   bootstrap:    [ AppComponent ],
   providers: [CoinsAppService]
 })
